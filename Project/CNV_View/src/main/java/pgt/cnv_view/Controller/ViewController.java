8f2b34c2811cb4ef54655e2774e852921e442c32
TranVuDucHuy <22021111@vnu.edu.vn>
2025-08-17 23:19:31 +0700

    Basic UI

--- Project/CNV_View/src/main/java/pgt/cnv_view/Controller/ViewController.java
+++ Project/CNV_View/src/main/java/pgt/cnv_view/Controller/ViewController.java
@@ -4,14 +4,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import javafx.event.ActionEvent;
 
 public class ViewController implements Initializable {
     @FXML
@@ -21,21 +24,45 @@ public class ViewController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
     }
 
-    public void page1(javafx.event.ActionEvent actionEvent) throws IOException {
+    public void page1(MouseEvent mouseEvent) throws IOException {
         Parent fxml = FXMLLoader.load(getClass().getResource("/pgt/cnv_view/FXML/page1.fxml"));
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
     }
 
-    public void page2(javafx.event.ActionEvent actionEvent) throws IOException {
+    public void page2(MouseEvent mouseEvent) throws IOException {
         Parent fxml = FXMLLoader.load(getClass().getResource("/pgt/cnv_view/FXML/page2.fxml"));
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
     }
 
-    public void page3(javafx.event.ActionEvent actionEvent) throws IOException {
+    public void page3(MouseEvent mouseEvent) throws IOException {
         Parent fxml = FXMLLoader.load(getClass().getResource("/pgt/cnv_view/FXML/page3.fxml"));
         contentArea.getChildren().removeAll();
         contentArea.getChildren().setAll(fxml);
+    }
+
+    public void scatterChart(ActionEvent actionEvent) throws IOException {
+        Parent fxml = FXMLLoader.load(getClass().getResource("/pgt/cnv_view/FXML/ScatterChart.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(fxml);
+    }
+
+    public void dataTable(ActionEvent actionEvent) throws IOException {
+        Parent fxml = FXMLLoader.load(getClass().getResource("/pgt/cnv_view/FXML/DataTable.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(fxml);
+    }
+
+    public void addSample(ActionEvent actionEvent) throws IOException {
+        Parent fxml = FXMLLoader.load(getClass().getResource("/pgt/cnv_view/FXML/AddSample.fxml"));
+
+        Scene scene = new Scene(fxml);
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Add Sample");
+        primaryStage.setScene(scene);
+        primaryStage.initModality(javafx.stage.Modality.APPLICATION_MODAL);
+        primaryStage.initOwner(contentArea.getScene().getWindow());
+        primaryStage.show();
     }
 }
